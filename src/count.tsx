bee76709d749100cd8bfd5b4f63e7bbb3d9da6b6
Melciya-033 <76159137+Melciya-033@users.noreply.github.com>
2021-03-01 19:49:34 -1100

    Update count.tsx
    Random color added

--- src/count.tsx
+++ src/count.tsx
@@ -14,10 +14,21 @@ function Count(){
           
     }
   
-    
+ function getRandomColor() {
+    const letters = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+
+  
    return(
         <div className="Index">
-        <h1 className="Counter">{count} </h1>
+       <h1 style={{ color: getRandomColor() }} className="Counter">
+        {count}{" "}
+      </h1>
         <button className="Inc-button" onClick={incCount}>+</button>
         <button className="Dec-button" onClick={decCount}>-</button>
 
